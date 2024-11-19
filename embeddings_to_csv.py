f3e732e49f115257fdda7c172168c25a1d4ecf98
joeDespres <100436066+joeDespres@users.noreply.github.com>
2024-11-19 11:03:28 -0500

    move rownames to column

--- embeddings_to_csv.py
+++ embeddings_to_csv.py
@@ -10,7 +10,10 @@ def main():
 
     df = DataFrame(embeddings).T
     df.columns = [f"V{i+1}" for i in range(df.shape[1])]
-    df.to_csv("./output/flavor_embeddings.csv", index=True)
+    df.index.name = "flavor"
+    df.reset_index(inplace=True)
+
+    df.to_csv("./output/flavor_embeddings.csv", index=False)
 
 
 if __name__ == "__main__":
